d4271b9547191824728c4b917e6e5c8a5bee9805
jonathans199 <jso1919@gmail.com>
2021-07-08 16:04:07 -0400

    added filtered function

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ mongoose
   .catch(err => console.log('Could not connect to MongoDb', err))
 
 const movieSchema = mongoose.Schema({
-  title: String,
+  title: { type: String, required: true, unique: true },
   genre: [String],
+  year: Number,
+  rating: Number,
   date: {
     type: Date,
     default: Date.now(),
@@ -23,4 +25,39 @@ function getAllMovies() {
     .catch(err => console.log('could not get all movies', err))
 }
 
-getAllMovies()
+// getAllMovies()
+
+function createMovie() {
+  const newMovie = new Movie({
+    title: 'Toy Story 3',
+    genre: ['Cartoons', 'Family'],
+    year: 2011,
+    rating: 5,
+  })
+  newMovie
+    .save()
+    .then(() => console.log('movie was saved'))
+    .catch(err => console.log('movie was not added', err))
+}
+
+// createMovie()
+
+function getAllMoviesCount() {
+  Movie.find()
+    .countDocuments()
+    .then(count => console.log('here is my count', count))
+    .catch(err => console.log(err))
+}
+
+// getAllMoviesCount()
+
+function getMoviesFiltered() {
+  Movie.find()
+    .limit(10)
+    .sort({ year: -1 })
+    .then(movies => console.log('here are filtered', movies))
+    .catch(err => console.log(err))
+}
+getMoviesFiltered()
+
+
